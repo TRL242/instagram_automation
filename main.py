fe8baca4d1374e15a8851f6ce2efa7d0669dbf50
trl242 <royaurallockhart@gmail.com>
2021-02-28 08:47:43 -0800

    added find followers, and follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 from time import sleep, time
 
 CHROME_DRIVER_PATH = YOUR CHROME DRIVER PATH
@@ -31,10 +32,34 @@ class InstaFollower:
         password.send_keys(Keys.ENTER)
 
     def find_followers(self):
-        pass
+        time.sleep(5)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+
+        time.sleep(2)
+        followers = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        followers.click()
+
+        time.sleep(2)
+        modal = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as a HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
 
     def follow(self):
-        pass
+        def follow(self):
+            all_buttons = self.driver.find_elements_by_css_selector("li button")
+            for button in all_buttons:
+                try:
+                    button.click()
+                    time.sleep(1)
+                except ElementClickInterceptedException:
+                    cancel_button = self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')
+                    cancel_button.click()
 
 
 bot = InstaFollower(CHROME_DRIVER_PATH)
